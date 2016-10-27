e9b1484e8158966c0ca891aec27973909b6a8f8e
NikTRSK <trsk.nick@gmail.com>
2016-10-26 20:26:51 -0700

    basic netwroking server

--- trajkovs_CSCI201L_Assignment4/src/networking/GameServer.java
+++ trajkovs_CSCI201L_Assignment4/src/networking/GameServer.java
@@ -1,5 +1,45 @@
 package networking;
 
-public class GameServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
 
+import GUI.GameData;
+
+public class GameServer {
+	private Vector<ServerThread> playerThreads;
+	
+	public GameServer(int port) {
+		ServerSocket ss = null;
+		playerThreads = new Vector<ServerThread>();
+		try {
+			ss = new ServerSocket(port);
+			while (true){
+				System.out.println("Server started on port " + port + "\nWaiting for connections...");
+				Socket s = ss.accept();
+				System.out.println("connection from " + s.getInetAddress() + ":" + s.getPort());
+				
+				ServerThread st = new ServerThread(s, this);
+				playerThreads.addElement(st);
+			}
+		} catch (IOException ioe) { System.out.println("ioe: " + ioe.getMessage()); }
+	finally {
+			try {
+				if (ss != null) {
+					ss.close();
+				}
+			} catch (IOException ioe) {}
+		}
+	}
+	
+	public void sendGameDataToAllClients(GameData gd) {
+//		if (gd != null && cm.getMessage() != null) {
+//			synchronized(serverThreads) {
+//				for (ServerThread st : serverThreads) {
+//					st.sendMessage(cm);
+//				}
+//			}
+//		}
+	}
 }
--- trajkovs_CSCI201L_Assignment4/src/networking/ServerThread.java
+++ trajkovs_CSCI201L_Assignment4/src/networking/ServerThread.java
@@ -1,5 +1,63 @@
 package networking;
 
-public class ServerThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+import GUI.GameData;
+
+public class ServerThread extends Thread {
+	private Socket s;
+	private BufferedReader br;
+	private PrintWriter pw;
+	private GameServer gs;
+	
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	
+	public ServerThread(Socket s, GameServer gs) {
+		this.gs = gs;
+		this.s = s;
+		
+		try {
+//			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+//			pw = new PrintWriter(s.getOutputStream());
+			oos = new ObjectOutputStream(s.getOutputStream());
+			ois = new ObjectInputStream(s.getInputStream());
+			this.start();
+		} catch (IOException ioe) {
+			System.out.println("ioe in Server Thread(): " + ioe.getMessage());
+		}
+	}
+	
+	public void sendGameData(GameData gd) {
+		try {
+			oos.writeObject(gd);
+			oos.flush();
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	public void run() {
+		try {
+			while (true) {
+				GameData gd = (GameData)ois.readObject();
+				gs.sendGameDataToAllClients(gd);
+			}
+		} catch (IOException ioe) {
+			System.out.println("ioe in run(): " + ioe.getMessage());
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("cnfe in run(): " + cnfe.getMessage());
+		}
+		finally {
+			try {
+				if (pw != null) pw.close();
+				if (br != null) br.close();
+				if (s != null) s.close();
+			} catch (IOException ioe) {}
+		}
+	}
 }
